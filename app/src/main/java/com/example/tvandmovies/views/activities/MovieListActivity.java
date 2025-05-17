fe6755efec25d4784c3d773a574f662f8387674a
Yoceeee <joci.majlinger@gmail.com>
2025-05-17 19:03:17 +0200

    A MovieListActivity-ben most már binding-ot használok, kevesebb boiler plate van használva, a töltést jelző indikátor kapott saját függvény, illetve a RecyclreView is

--- app/src/main/java/com/example/tvandmovies/views/activities/MovieListActivity.java
+++ app/src/main/java/com/example/tvandmovies/views/activities/MovieListActivity.java
@@ -2,17 +2,15 @@ package com.example.tvandmovies.views.activities;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.tvandmovies.R;
 import com.example.tvandmovies.controllers.MovieController;
+import com.example.tvandmovies.databinding.ActivityMainBinding;
 import com.example.tvandmovies.model.Movie;
 import com.example.tvandmovies.views.adapter.MovieAdapter;
 
@@ -21,71 +19,68 @@ import java.util.List;
 
 public class MovieListActivity extends AppCompatActivity {
     private MovieController movieController;
-
-    private RecyclerView popularMovieRecyclerView;
-    private RecyclerView newMovieRecyclerView;
-
     private MovieAdapter popularMovieAdapter;
     private MovieAdapter newMovieAdapter;
-
-    ProgressBar progressBarNew;
-    ProgressBar progressBarPop;
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // tötltés jelző példányosítás, beállítása láthatóra
-        progressBarPop = findViewById(R.id.popProgressBar);
-        progressBarNew = findViewById(R.id.newProgressBar);
+        // teljes képernyős megjelenítés
+        setupWindowFlags();
 
-        progressBarPop.setVisibility(View.VISIBLE);
-        progressBarNew.setVisibility(View.VISIBLE);
+        // a tötltés jelző beállítása láthatóra
+        setLoading(true);
 
-        // Népszerű filmek RecyclerView | vízszintes elrendezés beállítása
-        popularMovieRecyclerView = findViewById(R.id.recyclerViewPopularMovie);
-
-        popularMovieRecyclerView.setLayoutManager(
-                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        );
-        // a túlpörgetés miatt bekövetkező hullámzást tiltja
-        popularMovieRecyclerView.setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
+        // adapterek inicializálása
         popularMovieAdapter = new MovieAdapter(new ArrayList<>());
-        popularMovieRecyclerView.setAdapter(popularMovieAdapter);
-
-
-        // új filmek RecyclerView
-        newMovieRecyclerView = findViewById(R.id.recyclerViewNewMovie);
-        newMovieRecyclerView.setLayoutManager(
-                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        );
-        newMovieRecyclerView.setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
         newMovieAdapter = new MovieAdapter(new ArrayList<>());
-        newMovieRecyclerView.setAdapter(newMovieAdapter);
+
+        initRecyclerView(binding.recyclerViewPopularMovie, popularMovieAdapter);
+        initRecyclerView(binding.recyclerViewNewMovie, newMovieAdapter);
 
         // Filmek betöltése
         movieController = new MovieController(this);
         movieController.loadMovies();
 
-        // nem lesz sáv az app tetején
-        Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    // a töltést jelző bar egyszerűbb használata
+    private void setLoading(boolean isLoading) {
+        binding.popProgressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+        binding.newProgressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+    }
+
+    // A recyclerView-t itt inicializálom, mert több helyen kelleni fog
+    private void initRecyclerView(RecyclerView recyclerView, MovieAdapter adapter){
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        );
+        // a túlpörgetés miatt bekövetkező hullámzást tiltja
+        recyclerView.setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        recyclerView.setAdapter(adapter);
+    }
+
+    // nem fog megjelenni sáv az app tetején
+    public void setupWindowFlags(){
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
     }
 
     // Népszerű filmek beállítása az adapter segítségével
     public void updatePopularMovieList(List<Movie> movies) {
         popularMovieAdapter.setMovieList(movies);
-        progressBarPop.setVisibility(View.GONE); // amint megjelennek a filmek eltünik a progbar
+        setLoading(false); // amint megjelennek a filmek eltünik a progbar
     }
 
     public void updateNewMovies(List<Movie> movies){
         newMovieAdapter.setMovieList(movies);
-        progressBarNew.setVisibility(View.GONE);
+        setLoading(false);
     }
 
     // toast message küldése hiba esetén
